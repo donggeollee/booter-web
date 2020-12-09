b7597f5da9c8e35e4a4420b4d21f37a346c47333
e-build <donggeol92@gmail.com>
2020-12-10 02:24:31 +0900

    next <-> redux, saga 셋업 완료

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,15 @@
-/*!
-
-=========================================================
-* NextJS Material Kit v1.1.0 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
-
 import PageChange from "components/PageChange/PageChange.js";
-
 import "assets/scss/nextjs-material-kit.scss?v=1.1.0";
+
+// REDUX, REDUX-SAGA SETUP 
+import wrapper from "../store";
+import withReduxSaga from "next-redux-saga";
+
 
 Router.events.on("routeChangeStart", url => {
   console.log(`Loading: ${url}`);
@@ -42,27 +28,8 @@ Router.events.on("routeChangeError", () => {
   document.body.classList.remove("body-page-transition");
 });
 
-export default class MyApp extends App {
-  componentDidMount() {
-    let comment = document.createComment(`
+class MyApp extends App {
 
-=========================================================
-* NextJS Material Kit v1.1.0 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-`);
-    document.insertBefore(comment, document.documentElement);
-  }
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
 
@@ -72,16 +39,19 @@ export default class MyApp extends App {
 
     return { pageProps };
   }
+
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <React.Fragment>
-        <Head>
-          <title>NextJS Material Kit by Creative Tim</title>
-        </Head>
-        <Component {...pageProps} />
-      </React.Fragment>
+        <React.Fragment>
+          <Head>
+            <title>NextJS Material Kit by Creative Tim</title>
+          </Head>
+          <Component {...pageProps} />
+        </React.Fragment>
     );
   }
 }
+
+export default wrapper.withRedux(withReduxSaga(MyApp));
